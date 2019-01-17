29e286591ba8da3164d47ae92411b4abcb36a98e
yijialuo <https://git.oschina.net/xiaojianren/calculator2.0.git>
2019-01-17 10:37:06 +0800

    0116

---
+++ src/main/java/com/sq/demo/mapper/DepartmentMapper.java
@@ -0,0 +1,7 @@
+package com.sq.demo.mapper;
+
+import com.sq.demo.pojo.Department;
+import com.sq.demo.utils.MyMapper;
+
+public interface DepartmentMapper extends MyMapper<Department> {
+}
---
+++ src/main/java/com/sq/demo/mapper/PositionMapper.java
@@ -0,0 +1,7 @@
+package com.sq.demo.mapper;
+
+import com.sq.demo.pojo.Position;
+import com.sq.demo.utils.MyMapper;
+
+public interface PositionMapper extends MyMapper<Position> {
+}
